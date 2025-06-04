09cc83cd1b5a00bf914dc13d326a9756e5f261cd
Mouad <mouadaitouchen48@gmail.com>
2025-06-04 04:30:24 +0100

    Ajouter_7

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import './index.css';
+import './styles.css'
 import App from './App.jsx';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
