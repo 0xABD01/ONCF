5bc8b9ce8e285fa556c92f21d71ea0d05233cbe4
0xABD01 <Abderrazzak.esl@gmail.com>
2025-06-04 05:17:42 +0100

    Initial commit: ONCF project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 // src/App.js
 import React from 'react';
-import Navbar from './header/navbar';
-import Validation from './validation.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './header/navbar';
 import Home from './pages/page1/home.jsx';
 import Footer from './pages/page1/footer.jsx';
 import Compte from './pages/Compte/compte.jsx';
-import MotDePasseOublie from './pages/Compte/mdpoublier.jsx';
-import Shope from './pages/shope.jsx';
-import APropos from './pages/information/info.jsx';
-import Famille from './pages/offres/Famille.jsx';
-import Navette from './pages/offres/Navette.jsx';
-import Accessibilite from './pages/offres/Accessibilite.jsx';
-import Abonnement from './pages/Abonnement/Abonnement.jsx';
+import Validation from './validation.jsx';
 import Offres from './pages/page1/offres.jsx';
-import EarlyBooking from './pages/offres/EarlyBooking.jsx';
-
 
 const App = () => {
   return (
@@ -25,20 +16,10 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/compte' element={<Compte />} />
-          <Route path='/compte/motdepasse-oublie' element={<MotDePasseOublie/>}/>
           <Route path="/validation" element={<Validation />} />
-          <Route path="/shope" element={<Shope />} />
-          <Route path="/info" element={<APropos />} />
-
-          <Route path="/Offres/Famille" element={<Famille />} />
-          <Route path="/Offres/Navette" element={<Navette />} />
-          <Route path="/Offres/Accessibilité" element={<Accessibilite />} />
-          <Route path="/abonnements" element={<Abonnement />} />
           <Route path="/Offres" element={<Offres />} />
-          <Route path="/Offres/EarlyBooking" element={<EarlyBooking />} />
-    
         </Routes>
-        <Footer/>
+        <Footer />
       </>
     </BrowserRouter>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from './app/store.jsx';
 import './index.css';
 import './styles.css'
 import App from './App.jsx';
